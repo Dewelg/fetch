9598abd2449751c49663880ba108f047db427a9e
Dewelg <dewel713@gmail.com>
2023-01-24 09:50:01 -0500

    all done

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-console.log("Hello World!\n==========\n");
+let search = document.getElementById("searchWord");
+let searchbtn = document.getElementById("submitSearch");
+let feedbackEle = document.getElementById("feedback");
+let img = document.querySelector("img");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+searchbtn.addEventListener('click', () => {
+    fetch(
+        "https://api.giphy.com/v1/gifs/translate?api_key=OQykoV775syAmzUmQG1fQUvOkD63vwq6=" 
+        + search.value,
+        {mode: 'cors'}
+)
+.then((response) => response.json())
+.then((res) => {
+    feedbackEle.textContent = "";
+    img.src = res.data.images.original.url;
+    search.value = "";
+    })
+    .catch((err) => {
+      console.error(err);
+      feedbackEle.textContent = err.message;
+    });
+
+});
+
